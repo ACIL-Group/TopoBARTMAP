6f3b45ebb3b1c921db5bb17dccb7280948e016e2
RjY <43014672+Ray11641@users.noreply.github.com>
2023-03-14 11:59:25 -0500

    Added run_TopoBARTMAP_Unsupervised.m

---
+++ Functions/run_TopoBARTMAP_Unsupervised.m
@@ -0,0 +1,51 @@
+function [y, TBM] = run_TopoBARTMAP_Unsupervised(x, data)
+
+%{
+    :param x: array of settings.
+    :param data: array of shape #genesX#observations.
+    :param classes: array of true observation labels for the presented data.
+%}
+
+%% Data info
+    [nSamples_B, nSamples_A] = size(data);
+
+%% Parameters for TopoART module A
+    Settings = struct();
+    Settings.A = struct();
+    Settings.A.nFA = 1;                     %number of Fuzzy ART Layers in TopoARTA, >1 if doing hierarchical.
+    Settings.A.V = x(1);                    %vigilanceA
+    Settings.A.al = 1e-3;                   %alphaA
+    Settings.A.be = 1;                      %beta
+    Settings.A.bSB = x(2);                  %beta Second Best
+    Settings.A.ph = round(x(3));            %Phi for minimum number of members for not deleting a cluster
+    Settings.A.ta = round(x(4)*nSamples_A);
+    %     Settings.A.ta = max(Settings.A.ph, round(x(4)*nSamples_A)); %Number of cycles before the deletion happens in A
+
+%% Parameters for TopoART module B
+    Settings.B = struct();
+    Settings.B.nFA = 1;                     %number of Fuzzy ART LAyers in TopoARTB, >1 if doing hierarchical
+    Settings.B.V = x(5);                    %vigilanceB
+    Settings.B.al = 1e-3;                   %Alpha
+    Settings.B.be = 1;                      %beta
+    Settings.B.bSB = x(6);                  %beta Second Best
+    Settings.B.ph = round(x(7));            %Phi is number of minimum members in the cluster
+    Settings.B.ta = round(x(8)*nSamples_B);
+    %     Settings.B.ta = max(Settings.B.ph, round(x(8)*nSamples_B)); %Number of cycles before the deletion happens in B
+
+    %% Other parameters
+    Settings.cTh = x(9);                %Correlation threshold
+    Settings.cFn = 'pearson';           %Correlation function
+    Settings.sS = 0.01;                 %This can be used as parameter
+    Settings.noiseLabel = 'true';       %should the noise be labelled or not?
+
+%% Train
+    TBM = TopoBARTMAP(Settings);
+    TBM.Train(data');                   %because of the orientiation of input
+
+%% Compute ARI
+    perf = valid_internal_deviation(data, TBM.TAa.Label(1)', 1)
+
+%% Return fitness value [DB,CH,Dunn,KL,Han,st]
+    y = perf(2);
+    
+end
